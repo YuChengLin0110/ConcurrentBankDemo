c2b4c9fe4e386a746183b14d0a81275ef5aca46a
YuChengLin0110 <aa46821564@gmail.com>
2025-04-17 01:02:31 +0800

    使用Atomic 改寫Account
    將 ReentrantLock 改為 AtomicLong
    修改withdraw()、deposit()、getBalance()

--- src/main/java/com/example/model/Account.java
+++ src/main/java/com/example/model/Account.java
@@ -1,37 +1,41 @@
 package com.example.model;
 
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Account {
 	private final int id;
-	private long balance;
+	private AtomicLong balance;
 	private final ReentrantLock lock = new ReentrantLock();
 
-	public Account(int id, Long balance) {
+	public Account(int id, AtomicLong balance) {
 		this.id = id;
 		this.balance = balance;
 	}
 
 	public void deposit(long amount) {
-		lock.lock();
-		try {
-			balance += amount;
-		} finally {
-			lock.unlock();
-		}
+		balance.addAndGet(amount);
 	}
-
+	
+	/* 使用Atomic
+	 * 先讀取目前balance
+	 * 計算好新的balance
+	 * 使用compareAndSet : 檢查當前 balance 是否與讀取到的值相同
+	 * true : 則更新為新的balance ， false : 則balance被修改過，重新讀取計算
+	 * */
 	public boolean withdraw(long amount) {
-		lock.lock();
-		try {
-			if (balance >= amount) {
-				balance -= amount;
-				return true;
-			} else {
+		while(true) {
+			long curr = balance.get();
+			
+			if(curr < amount) {
 				return false;
 			}
-		} finally {
-			lock.unlock();
+			
+			long newBalance = curr - amount;
+			
+			if(balance.compareAndSet(curr, newBalance)) {
+				return true;
+			}
 		}
 	}
 
@@ -40,12 +44,7 @@ public class Account {
 	}
 
 	public long getBalance() {
-		lock.lock();
-		try {
-			return balance;
-		} finally {
-			lock.unlock();
-		}
+		return balance.get();
 	}
 
 	public ReentrantLock getLock() {
